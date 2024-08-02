4d5360e441116ab6d88987ffccf778f5fdb4b576
Stanislav <sv_golang@mail.ru>
2024-08-02 16:26:31 +0200

    add a provider

---
+++ providers.tf
@@ -0,0 +1,16 @@
+# Source: https://registry.terraform.io/providers/terrabitz/wasabi/latest
+
+terraform {
+  required_providers {
+    wasabi = {
+      source = "terrabitz/wasabi"
+      version = "4.1.3"
+    }
+  }
+}
+
+provider "wasabi" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
