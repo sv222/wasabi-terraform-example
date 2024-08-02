f35b0d15adddd1e482b9ab21a234973cdf48668d
Stanislav <sv_golang@mail.ru>
2024-08-02 16:27:01 +0200

    create users

---
+++ users.tf
@@ -0,0 +1,15 @@
+resource "wasabi_user" "alice" {
+  name = "alice"
+}
+
+resource "wasabi_user" "bob" {
+  name = "bob"
+}
+
+resource "wasabi_user" "charlie" {
+  name = "charlie"
+}
+
+resource "wasabi_user" "backup" {
+  name = "backup"
+}
