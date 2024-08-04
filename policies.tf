eaec35c8fc39396f9d101bce81681fbf08424415
Stanislav <sv_golang@mail.ru>
2024-08-04 23:37:00 +0200

    add reference for policies

--- policies.tf
+++ policies.tf
@@ -2,6 +2,8 @@
 and comparability with AWS S3
 
 # Read-Only Policy
+# Reference: https://docs.aws.amazon.com/aws-managed-policy/latest/reference/AmazonS3ReadOnlyAccess.html
+
 data "wasabi_policy_document" "read_only" {
   statement {
     sid = "ReadOnlyAccess"
@@ -28,6 +30,8 @@ data "wasabi_policy_document" "read_only" {
 }
 
 # Read-Write Policy
+# Reference: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_s3_rw-bucket.html
+
 data "wasabi_policy_document" "read_write" {
   statement {
     sid = "ReadWriteAccess"
